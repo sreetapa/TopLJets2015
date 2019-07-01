dd4e9b5973a68e8827f227c4eb51eddce5d6224d
Pedro <psilva@cern.ch>
2019-07-02 00:03:28 +0200

    adding skeleton for top 2019 summer students

--- TopAnalysis/test/analysis/summer2019/inspectPlotter.py
+++ TopAnalysis/test/analysis/summer2019/inspectPlotter.py
@@ -10,10 +10,10 @@ pList=[('inc_csi','inclusive',1),
        ('rp123_csi','RP=123',ROOT.kOrange+1),
        ('rp23_csi','RP=23',ROOT.kMagenta+2)]
 
-pname='mlb'
-pList=[('inc_mlb','inclusive',1),
-       ('plus_mlb','RP=123',ROOT.kOrange+1),
-       ('minus_mlb','RP=23',ROOT.kMagenta+2)]
+#pname='mlb'
+#pList=[('inc_mlb','inclusive',1),
+#       ('plus_mlb','#mu^{+}',ROOT.kOrange+1),
+#       ('minus_mlb','#mu^{-}',ROOT.kMagenta+2)]
 
 ROOT.gStyle.SetOptStat(0)
 ROOT.gStyle.SetOptTitle(0)
@@ -25,7 +25,7 @@ for p,title,color in pList:
     histos[-1].SetLineColor(color)
     histos[-1].SetTitle(title)
     histos[-1].Draw('same' if len(histos)>1 else 'hist')
-c.BuildLegend()
+c.BuildLegend(0.65,0.95,0.95,0.8)
 c.Modified()
 c.Update()
 c.SaveAs(pname+'.png')
